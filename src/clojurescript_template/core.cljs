2ffdbed5ad2b62bd24dfc0fafd4226c1970957c2
Ioannis Canellos <iocanel@gmail.com>
2021-10-15 21:51:40 +0300

    Initial commit

---
+++ src/clojurescript_template/core.cljs
@@ -0,0 +1,3 @@
+(ns clojurescript-template.core)
+
+(println "Hello, World!")
---
+++ test/clojurescript_template/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-template.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-template.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
