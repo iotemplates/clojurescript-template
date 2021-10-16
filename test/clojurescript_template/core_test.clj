06956bbcdcdad0da587b250c1c338f20cb31dd51
Ioannis Canellos <iocanel@gmail.com>
2021-10-16 22:39:16 +0300

    fixup: test

--- test/clojurescript_template/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-template.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-template.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
---
+++ test/clojurescript_template/core_test.cljs
@@ -0,0 +1,6 @@
+(ns ^:kaocha/focus clojurescript-template.core-test
+  (:require [cljs.test :as t :refer [is testing]]))
+
+(t/deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
